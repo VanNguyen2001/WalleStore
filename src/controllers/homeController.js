4f44425fa77adf4a09b97169b997b5a279070376
3S_ANHTUAN\duong <duonganhtuan162002@gmail.com>
2023-06-08 11:34:22 +0700

    .

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,8 +3,9 @@ import pool from '../configs/connectDB';
 let getHomePage = async (req, res) => {
     const [rows] = await pool.execute('SELECT * FROM category');
     const [roll] = await pool.execute('SELECT * FROM brand');
+    const [ron] = await pool.execute('SELECT * FROM product');
 
-    return res.render('pages/index.ejs', { dataCategory: rows, dataBrand: roll})
+    return res.render('pages/index.ejs', { dataCategory: rows, dataBrand: roll, dataProduct: ron })
 }
 
 let child = async (req, res) => {
